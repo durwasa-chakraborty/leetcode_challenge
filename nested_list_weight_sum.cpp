e881f8b9c06c18cd42c1db762223cd348c2943e1
Durwasa Chakraborty <durwasa@Durwasas-MacBook-Air.local>
2017-09-07 09:46:19 -0400

    LC Day 3

---
+++ nested_list_weight_sum.cpp
@@ -0,0 +1,32 @@
+
+#include <list>
+#include <algorithm>
+#include <iostream>
+
+/*
+.
+.
+.
+Solution 
+class Solution {
+private:
+    int DFS(std::vector<NestedIntegers>& nested_integers_list, int depth) {
+        int _size = (int)nested_integers_list.size();
+        int _sol = 0;
+        for (int i = 0; i < _size; ++i)
+            _sol+=(nested_integers_list[i].isInteger())?
+                    nested_integers_list[i]:
+                    DFS(nested_integers_list[i],depth+1);
+
+    
+    };
+
+public:
+    int depthSum(std::vector<NestedIntegers>& v) {
+        return DFS(v,1);
+    }
+
+
+};
+*/
+
---
+++ number_complement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Solution {
+public:
+    int findComplement(int num) {
+        int _xor = 2;
+        int _original_num = num;
+        while(num>>=1) 
+            _xor<<=1;
+
+        return _original_num^(_xor-1);
+    }
+};
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    Solution obj = Solution();
+    std::cout<<obj.findComplement(1)<<std::endl;
+    return 0;
+}
